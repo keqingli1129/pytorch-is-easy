8a724252b035cb4cea88396ffd93a2c7b330937c
Keqing Li <keqingli1129@gmail.com>
2025-12-27 16:35:31 -0600

    lora model

--- checkcuda.py
+++ checkcuda.py
@@ -2,13 +2,8 @@ import torch
 def get_device():
     return "cuda" if torch.cuda.is_available() else "cpu"
 def main():
-    # if torch.cuda.is_available():
-    #     print("CUDA is available.")
-    #     print(f"Number of CUDA devices: {torch.cuda.device_count()}")
-    #     print(f"Current CUDA device: {torch.cuda.current_device()}")
-    #     print(f"CUDA device name: {torch.cuda.get_device_name(torch.cuda.current_device())}")
-    # else:
-    #     print("CUDA is not available.")
-    print(get_device())
+    print(f"CUDA Available: {torch.cuda.is_available()}")
+    print(f"Device Name: {torch.cuda.get_device_name(0)}")
+    print(f"Capability: {torch.cuda.get_device_capability(0)}") # Should be (5, 0)
 if __name__ == "__main__":
     main()
